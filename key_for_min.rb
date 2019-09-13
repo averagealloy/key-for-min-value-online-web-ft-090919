80782d53872a875555cb6317eaf47f596ffe2358
Mike McTighe <m.mctighe10@gmail.com>
2019-09-13 20:55:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,15 @@ def key_for_min_value(name_hash)
     "#{name_hash[key]}".length <=> "#{name_hash[key]}".length
 
   end
-  
+  # prereqs: iterators, hashes, conditional logic
+# Given a hash with numeric values, return the key for the smallest value
+
+def key_for_min_value(name_hash)
+  name_hash.each do |key , value|
+    "#{name_hash[key]}".length <=> "#{name_hash[key]}".length
+
+  end
+  key_for_min_value(name_hash)
+end
+
 end
