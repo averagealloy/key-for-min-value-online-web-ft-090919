c836696fa2d3351ada0c9afdea09664dab1b1d7a
Mike McTighe <m.mctighe10@gmail.com>
2019-09-13 20:55:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@
 def key_for_min_value(name_hash)
   name_hash.each do |key , value|
     "#{name_hash[key]}".length <=> "#{name_hash[key]}".length
-    key_for_min_value(name_hash)
+
   end
+  
 end
